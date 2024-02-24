f0edf321da464def860a5d4dcad0c3cf967dc9cd
Huaiyuan Jing <ls.hyjing@gmail.com>
2024-02-24 17:10:18 -0600

    Update Ruby version and add Rubocop gem

--- Gemfile
+++ Gemfile
@@ -1,9 +1,9 @@
 source 'http://rubygems.org'
 
-ruby "2.6.6"
+ruby "3.2.3"
 
 gem 'rspec', '~> 3.8'
 gem 'cucumber', '2.0.0'
 
 gem 'byebug'
-
+gem 'rubocop', group: 'development', require: false
--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -1,35 +1,77 @@
+# frozen_string_literal: true
+
 # When done, submit this entire file to the autograder.
 
 # Part 1
 
-def sum arr
+def sum(arr)
   # YOUR CODE HERE
+  sum = 0
+  arr.each do |i|
+    sum += i
+  end
+  sum
 end
 
-def max_2_sum arr
+def max_2_sum(arr)
   # YOUR CODE HERE
+  sum = 0
+  arr = arr.sort
+  arr = arr.reverse
+  # print arr
+  sum += arr[0] if arr.length.positive?
+  sum += arr[1] if arr.length > 1
+  sum
 end
 
-def sum_to_n? arr, n
-  # YOUR CODE HERE
+def sum_to_n?(arr, num)
+  return false if arr.length <= 1
+
+  # print arr
+  # puts n
+  (0..arr.length - 1).each do |i|
+    (i + 1..arr.length - 1).each do |j|
+      # print i, j
+      return true if arr[i] + arr[j] == num
+    end
+  end
+  false
 end
 
 # Part 2
 
 def hello(name)
   # YOUR CODE HERE
+  "Hello, #{name}"
 end
 
-def starts_with_consonant? s
-  # YOUR CODE HERE
+def starts_with_consonant?(s)
+  print s
+  return false if s.empty?
+
+  return false unless s[0].is_a?(String) && s[0].match?(/[A-Za-z]/)
+
+  !%w[A E I O U a e i o u].include? s[0]
 end
 
-def binary_multiple_of_4? s
-  # YOUR CODE HERE
+def binary_multiple_of_4?(s)
+  s.match?(/^[1, 0]*0$/)
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  attr_accessor :isbn, :price
+
+  def initialize(isbn, price)
+    raise ArgumentError, 'ISBN number cannot be empty' if isbn.empty?
+    raise ArgumentError, 'Price must be greater than zero' if price <= 0
+
+    @isbn = isbn
+    @price = price
+  end
+
+  def price_as_string
+    format('$%.2f', @price)
+  end
 end
